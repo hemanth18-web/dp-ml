f9142052b9304efe8e87c25fcb63a97750a17a22
hemanth18-web <75420808+hemanth18-web@users.noreply.github.com>
2025-01-26 12:25:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 st.title('Predict Fare of Ailines Tickets')
 st.write("please enter the sourcce and destination for your travel")
-user_input = st.text_input("please enter your source:")
-user_destiantion = st.text_input("please enter your destination:")
-st.write("You entered:", user_input)
-st.link_button("Check the ticket fare", "https://streamlit.io/gallery")
+#user_input = st.text_input("please enter your source:")
+#user_destiantion = st.text_input("please enter your destination:")
+#st.write("You entered:", user_input)
+#st.link_button("Check the ticket fare", "https://streamlit.io/gallery")
 # Take input from the user
 user_input = st.text_input("Please enter your source:")
 user_destination = st.text_input("Please enter your destination:")
@@ -16,4 +16,4 @@ if st.button("Submit"):
     st.write("Destination:", user_destination)
     
     # Add a link button to check ticket fare
-    st.markdown("[Check the ticket fare](https://streamlit.io/gallery)")
+    #st.markdown("[Check the ticket fare](https://streamlit.io/gallery)")
