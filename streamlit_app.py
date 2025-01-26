98a7233ebfda0f7b4bccbf8c9cd9448ad635a14b
hemanth18-web <75420808+hemanth18-web@users.noreply.github.com>
2025-01-26 12:21:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 st.title('Predict Fare of Ailines Tickets')
 st.write("please enter the sourcce and destination for your travel")
+user_input = st.text_input("Enter some text:")
+st.write("You entered:", user_input)
 st.link_button("Check the ticket fare", "https://streamlit.io/gallery")
