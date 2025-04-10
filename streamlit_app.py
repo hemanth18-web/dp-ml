b5d8c2bfb7008a0382df5701eaaa1bcfac5a6aed
hemanth18-web <75420808+hemanth18-web@users.noreply.github.com>
2025-04-10 20:54:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ def load_data_from_github(url):
 data = load_data_from_github(github_url)
 
 # --- STREAMLIT APP ---
-st.title("Flight Fare Data Exploration and Prediction")
+st.title("Flight Fare Data Exploration and Prediction22")
 
 if data is not None:
     # --- Data Cleaning and Conversion ---
